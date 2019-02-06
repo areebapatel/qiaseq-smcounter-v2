51b03363d13d0743dfca7292c9ae32ce26e96f90
rpadmanabhan <raghus606@gmail.com>
2019-02-06 01:52:14 +0000

    Changes to support TumorNormal filters

--- vcf.py
+++ vcf.py
@@ -3,22 +3,26 @@ import os
 #-------------------------------------------------------------------------------------
 # assign genotype
 #-------------------------------------------------------------------------------------
-def assign_gt(alt,chrom,vmf):
+def assign_gt(alt,chrom,vmf,fltr):
    ''' Function for faking the Genotype i.e. GT field
    for downstream tools
    :param alts (str) alternative allele(s)
    :param chrom (str) chromosome the variant is on
    :param vmf (str) variant minor allele frequency (comma seperated for multi-allelic sites)
+   :param fltr (str) FILTER for this variant
    '''
-   alts = alt.split(",")
-   if len(alts) >= 2: ## Treat all multiallelic sites as heterozygotes for the first 2 variant alleles
-      genotype = '1/2'
-   elif chrom == "chrY" or chrom == "chrM":
-      genotype = '1'
-   elif float(vmf) > 0.95 : ## Treat as Heterozygous
-      genotype = '1/1'
+   if fltr == 'LOH_HomRef': # TumorNormal case
+      genotype = '0/0'
    else:
-      genotype = '0/1'
+      alts = alt.split(",")
+      if len(alts) >= 2: ## Treat all multiallelic sites as heterozygotes for the first 2 variant alleles
+         genotype = '1/2'
+      elif chrom == "chrY" or chrom == "chrM":
+         genotype = '1'
+      elif float(vmf) > 0.95 : ## Treat as Heterozygous
+         genotype = '1/1'
+      else:
+         genotype = '0/1'
 
    return genotype
 
@@ -39,98 +43,144 @@ def assign_ad(uumi,vumi):
 #--------------------------------------------------------------------------------------
 # function to handle normal variants
 #--------------------------------------------------------------------------------------
-def biAllelicVar(alleles, RepRegion, outVcf, outVariants):
+def biAllelicVar(alleles, RepRegion, outVcf, outVariants, tumorNormal):
    ID = '.'
-   chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fqual, fltr = alleles[0]
+   if tumorNormal:
+      chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fqual, fltr, fetpval = alleles[0]
+      INFO = ';'.join(
+         ['TYPE=' +typ, 'RepRegion='+RepRegion, 'TNFetPval='+fetpval,
+          'DP='+dp, 'UMT='+umt, 'VMT='+vmt, 'VMF='+vmf])
+   else:
+      chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fqual, fltr = alleles[0]
+      INFO = ';'.join(
+         ['TYPE='+typ, 'RepRegion='+RepRegion,
+          'DP='+dp, 'UMT='+umt, 'VMT='+vmt,'VMF='+vmf])
 
-   INFO = ';'.join(
-      ['TYPE=' +typ,'RepRegion=' + RepRegion,'DP='+dp,'UMT='+umt,'VMT='+vmt,
-      'VMF='+vmf]
-      ) 
    FORMAT = 'GT:AD:VF'
-   gt = assign_gt(alt,chrom,vmf)      
+   gt = assign_gt(alt,chrom,vmf,fltr)
    ad = assign_ad(umt,vmt)         
    SAMPLE = ':'.join([gt,ad,vmf])
+
+   if fltr.find('LOH_HomRef') != -1:
+      alt = '.'
+      fltr.replace('_HomRef','')
+
+   cutVarLine = '\t'.join([chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fltr]) + '\n'
    vcfLine = '\t'.join([chrom, pos, ID, ref, alt, qual, fltr, INFO, FORMAT, SAMPLE]) + '\n'
    outVcf.write(vcfLine)
-   cutVarLine = '\t'.join([chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fltr]) + '\n'
    outVariants.write(cutVarLine)
 
 #--------------------------------------------------------------------------------------
 # function to handle multi-allelic variants
 #--------------------------------------------------------------------------------------
-def multiAllelicVar(alleles, RepRegion, outVcf, outVariants):
+def multiAllelicVar(alleles, RepRegion, outVcf, outVariants, tumorNormal):
    ID = '.'
-   tmpAlleles = [x for x in alleles if x[-1] == 'PASS']
-   lenTmpAlleles = len(tmpAlleles)
-   if lenTmpAlleles == 0:
-     pass
-   elif lenTmpAlleles == 1:
-     chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fqual, fltr = tmpAlleles[0]
-     INFO = ';'.join(
-            ['TYPE=' +typ,'RepRegion=' + RepRegion,'DP='+dp,'UMT='+umt,'VMT='+vmt,
-             'VMF='+vmf]
-         )
-     FORMAT = 'GT:AD:VF'
-     gt = assign_gt(alt,chrom,vmf)
-     ad = assign_ad(umt,vmt)
-     SAMPLE = ':'.join([gt,ad,vmf])     
-     vcfLine = '\t'.join([chrom, pos, ID, ref, alt, qual, fltr, INFO, FORMAT, SAMPLE]) + '\n'
-     outVcf.write(vcfLine)
-     cutVarLine = '\t'.join([chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fltr]) + '\n'
-     outVariants.write(cutVarLine)
+   if tumorNormalVar:
+      fltr_col = -2
    else:
-     VDPs, VAFs, VMTs, UMTs, VMFs, QUALs, fQUALs, TYPEs, REFs, ALTs, DPs = [], [], [], [], [], [], [], [], [], [], []
-     for allele in tmpAlleles:
-        chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fqual, fltr = allele
-        VDPs.append(vdp)
-        VAFs.append(vaf)
-        VMTs.append(vmt)
-        UMTs.append(umt)
-        VMFs.append(vmf)
-        QUALs.append(qual)
-        TYPEs.append(typ)
-        REFs.append(ref)
-        ALTs.append(alt)
-        fQUALs.append(fqual)
-        DPs.append(dp)
+      fltr_col = -1
 
-     # debug check
-     assert len(set(UMTs)) == 1, "The number of used UMIs at a site should be the same across all alleles"
+   tmpAlleles = [x for x in alleles if x[fltr_col] == 'PASS']
+   lenTmpAlleles = len(tmpAlleles)
 
-     # align multiple alleles to the same REF if necessary
-     if all(x==REFs[0] for x in REFs):
-        finalRef = REFs[0]
-        finalAlt = ','.join(ALTs)
-     else:
-        # Assumption: the first bases are the same 
-        finalRef = max(REFs, key=len)
-        for j in range(len(ALTs)):
-           ALTs[j] = ALTs[j] if REFs[j] == finalRef else ALTs[j] + finalRef[len(REFs[j]):]
-        finalAlt = ','.join(ALTs)
+   if lenTmpAlleles == 0:
+      pass
 
-     newQual = str(min(fQUALs))
-     allTypes = ','.join(TYPEs)
-     allVDPs = ','.join(VDPs)
-     allVAFs = ','.join(VAFs)
-     allVMTs = ','.join(VMTs)
-     allVMFs = ','.join(VMFs)
-     allDPs = ','.join(DPs)
+   elif lenTmpAlleles == 1:
 
-     INFO = ';'.join(['TYPE=' +allTypes,'RepRegion=' + RepRegion,'DP='+allDPs,'UMT='+umt,'VMT='+allVMTs,'VMF='+allVMFs])
-     FORMAT = 'GT:AD:VF' 
-     gt = assign_gt(finalAlt,chrom,allVMFs)
-     ad = assign_ad(umt,allVMTs)   
-     SAMPLE = ':'.join([gt,ad,allVMFs])
-     vcfLine = '\t'.join([chrom, pos, ID, finalRef, finalAlt, newQual, 'PASS', INFO, FORMAT, SAMPLE]) + '\n'     
-     outVcf.write(vcfLine)
-     cutVarLine = '\t'.join([chrom, pos, finalRef, finalAlt, allTypes, allDPs, allVDPs, allVAFs, umt, allVMTs, allVMFs, newQual,'PASS']) + '\n'
-     outVariants.write(cutVarLine)
+      if tumorNormal:
+         chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fqual, fltr, fetpval = alleles[0]
+         INFO = ';'.join(
+            ['TYPE=' +typ, 'RepRegion='+RepRegion, 'TNFetPval='+fetpval,
+             'DP='+dp, 'UMT='+umt, 'VMT='+vmt, 'VMF='+vmf])
+      else:
+         chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fqual, fltr = tmpAlleles[0]
+         INFO = ';'.join(
+            ['TYPE=' +typ, 'RepRegion=' + RepRegion,
+             'DP='+dp, 'UMT='+umt,'VMT='+vmt, 'VMF='+vmf])
+
+      FORMAT = 'GT:AD:VF'
+      gt = assign_gt(alt,chrom,vmf,fltr)
+      ad = assign_ad(umt,vmt)
+      SAMPLE = ':'.join([gt,ad,vmf])
+
+      if fltr.find('LOH_HomRef') != -1:
+         alt = '.'
+         fltr.replace('_HomRef','')
+
+      vcfLine = '\t'.join([chrom, pos, ID, ref, alt, qual, fltr, INFO, FORMAT, SAMPLE]) + '\n'
+      outVcf.write(vcfLine)
+      cutVarLine = '\t'.join([chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fltr]) + '\n'
+      outVariants.write(cutVarLine)
+
+   else:
+      VDPs, VAFs, VMTs, UMTs, VMFs, QUALs, fQUALs, TYPEs, REFs, ALTs, DPs, PVals = [], [], [], [], [], [], [], [], [], [], [], []
+      for allele in tmpAlleles:
+         if tumorNormal:
+            chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fqual, fltr, fetpval = allele
+            Pvals.append(fetpval)
+         else:
+            chrom, pos, ref, alt, typ, dp, vdp, vaf, umt, vmt, vmf, qual, fqual, fltr = allele
+         VDPs.append(vdp)
+         VAFs.append(vaf)
+         VMTs.append(vmt)
+         UMTs.append(umt)
+         VMFs.append(vmf)
+         QUALs.append(qual)
+         TYPEs.append(typ)
+         REFs.append(ref)
+         ALTs.append(alt)
+         fQUALs.append(fqual)
+         DPs.append(dp)
+
+      # debug check
+      assert len(set(UMTs)) == 1, "The number of used UMIs at a site should be the same across all alleles"
+
+      # align multiple alleles to the same REF if necessary
+      if all(x==REFs[0] for x in REFs):
+         finalRef = REFs[0]
+         finalAlt = ','.join(ALTs)
+      else:
+         # Assumption: the first bases are the same
+         finalRef = max(REFs, key=len)
+         for j in range(len(ALTs)):
+            ALTs[j] = ALTs[j] if REFs[j] == finalRef else ALTs[j] + finalRef[len(REFs[j]):]
+
+         finalAlt = ','.join(ALTs)
+
+      newQual = str(min(fQUALs))
+      allTypes = ','.join(TYPEs)
+      allVDPs = ','.join(VDPs)
+      allVAFs = ','.join(VAFs)
+      allVMTs = ','.join(VMTs)
+      allVMFs = ','.join(VMFs)
+      allDPs = ','.join(DPs)
+
+      if tumorNormal:
+         allPvals = ','.join(Pvals)
+         INFO = ';'.join(['TYPE=' +allTypes,'RepRegion=' + RepRegion,
+                          'TNFetPval=' + allPvals,
+                          'DP='+allDPs,'UMT='+umt,'VMT='+allVMTs,'VMF='+allVMFs])
+      else:
+         INFO = ';'.join(['TYPE=' +allTypes,'RepRegion=' + RepRegion,
+                          'DP='+allDPs,'UMT='+umt,'VMT='+allVMTs,'VMF='+allVMFs])
+
+      # debug check
+      assert fltr == 'PASS', "Error in assinging FILTER at Multi-Allelic Site"
+
+      FORMAT = 'GT:AD:VF'
+      gt = assign_gt(finalAlt,chrom,allVMFs,fltr)
+      ad = assign_ad(umt,allVMTs)
+      SAMPLE = ':'.join([gt,ad,allVMFs])
+      vcfLine = '\t'.join([chrom, pos, ID, finalRef, finalAlt, newQual, 'PASS', INFO, FORMAT, SAMPLE]) + '\n'
+      outVcf.write(vcfLine)
+      cutVarLine = '\t'.join([chrom, pos, finalRef, finalAlt, allTypes, allDPs, allVDPs, allVAFs, umt, allVMTs, allVMFs, newQual,'PASS']) + '\n'
+      outVariants.write(cutVarLine)
 
 #--------------------------------------------------------------------------------------
 # main function
 #--------------------------------------------------------------------------------------
-def makeVcf(runPath, outlong, sampleName, refGenome):
+def makeVcf(runPath, outlong, sampleName, refGenome, tumorNormal = False):
    # change working directory to runDir
    os.chdir(runPath)
    outAll = open(sampleName + '.smCounter.all.txt', 'w')
@@ -142,9 +192,16 @@ def makeVcf(runPath, outlong, sampleName, refGenome):
    minCutoff = {'INDEL': 2,'SNP':2} ## Cutoff for the low-PI file
    
    ID = '.'
+
    headerAll = ['CHROM', 'POS', 'REF', 'ALT', 'TYPE', 'sUMT', 'sForUMT', 'sRevUMT', 'sVMT', 'sForVMT', 'sRevVMT', 'sVMF', 'sForVMF', 'sRevVMF', 'VDP', 'VAF', 'RefForPrimer', 'RefRevPrimer', 'primerOR', 'pLowQ', 'hqUmiEff', 'allUmiEff', 'refMeanRpb', 'altMeanRpb', 'rpbEffectSize', 'repType', 'hpInfo', 'simpleRepeatInfo', 'tandemRepeatInfo', 'DP', 'FR', 'MT', 'UFR', 'sUMT_A', 'sUMT_T', 'sUMT_G', 'sUMT_C', 'logpval', 'FILTER']
    headerVariants = ['CHROM','POS','REF','ALT','TYPE','DP','VDP','VAF','sUMT','sVMT','sVMF','QUAL','FILTER']
-   headerLowPi = ['READ_SET','CHROM','POS','ID','REF','ALT','QUAL','FILTER','TYPE','RepRegion','DP','UMT','VMT','VMF']
+
+   if tumorNormal:
+      headerAll.append('TNFetPval')
+      headerLowPi = ['READ_SET','CHROM','POS','ID','REF','ALT','QUAL','FILTER','TYPE','RepRegion','TNFetPval','DP','UMT','VMT','VMF']
+   else:
+      headerLowPi = ['READ_SET','CHROM','POS','ID','REF','ALT','QUAL','FILTER','TYPE','RepRegion','DP','UMT','VMT','VMF']
+
    
    headerVcf = '##fileformat=VCFv4.2' + '\n' + \
          '##reference={ref}'.format(ref=refGenome) + '\n' + \
@@ -161,9 +218,21 @@ def makeVcf(runPath, outlong, sampleName, refGenome):
          '##FILTER=<ID=RBCP,Description="Variant are clustered at the end of barcode-side reads">' + '\n' + \
          '##FILTER=<ID=RPCP,Description="Variant are clustered at the end of primer-side reads">' + '\n' + \
          '##FILTER=<ID=PB,Description="Primer bias filter. odds ratio > 10 or < 0.1">' + '\n' + \
-         '##FILTER=<ID=PrimerCP,Description="variant is clustered within 2 bases from primer sequence due to possible primer dimers">' + '\n' + \
+         '##FILTER=<ID=PrimerCP,Description="variant is clustered within 2 bases from primer sequence due to possible primer dimers">' + '\n'
+
+   if tumorNormal:
+      headerVcf = headerVcf + \
+         '##FILTER=<ID=LOH,Description="Loss of Heterozygocity">' + '\n' \
+         '##FILTER=<ID=Germline_Risk,Description="Not a significant difference in UMI counts for the variant allele between Normal and Tumor Samples">' + '\n'
+   headerVcf = headerVcf + \
          '##INFO=<ID=TYPE,Number=.,Type=String,Description="Variant type: SNP/INDEL/COMPLEX">' + '\n' + \
-         '##INFO=<ID=RepRegion,Number=.,Type=String,Description="Repetitive region">' + '\n' + \
+         '##INFO=<ID=RepRegion,Number=.,Type=String,Description="Repetitive region">' + '\n'
+
+   if tumorNormal:
+      headerVcf = headerVcf + \
+         '##INFO=<ID=TNFetPval,Number=.,Type=Float,Description="FDR Corrected pvalue from Fischer\'s Exact Test">' + '\n'
+
+   headerVcf = headerVcf + \
          '##INFO=<ID=DP,Number=1,Type=Integer,Description="Total read depth">' + '\n' + \
          '##INFO=<ID=UMT,Number=1,Type=Integer,Description="Total used UMI depth">' + '\n' + \
          '##INFO=<ID=VMT,Number=.,Type=Integer,Description="Variant UMI depth">' + '\n' + \
@@ -185,9 +254,15 @@ def makeVcf(runPath, outlong, sampleName, refGenome):
    with open(outlong, 'r') as f:
       next(f)
       for line in f:
-         outAll.write(line)
+         tempLine = line
+         if tempLine.find('LOH_HomRef') != -1:
+            tempLine = tempLine.replace('_HomRef','')
+         outAll.write(tempLine)
          cnt += 1
-         CHROM, POS, REF, ALT, TYPE, sUMT, sForUMT, sRevUMT, sVMT, sForVMT, sRevVMT, sVMF, sForVMF, sRevVMF, VDP, VAF, RefForPrimer, RefRevPrimer, primerOR, pLowQ, hqUmiEff, allUmiEff, refMeanRpb, altMeanRpb, rpbEffectSize, repType, hpInfo, simpleRepeatInfo, tandemRepeatInfo, DP, FR, MT, UFR, sUMT_A, sUMT_T, sUMT_G, sUMT_C, logpval, FILTER = line.strip().split('\t')
+         if tumorNormal:
+            CHROM, POS, REF, ALT, TYPE, sUMT, sForUMT, sRevUMT, sVMT, sForVMT, sRevVMT, sVMF, sForVMF, sRevVMF, VDP, VAF, RefForPrimer, RefRevPrimer, primerOR, pLowQ, hqUmiEff, allUmiEff, refMeanRpb, altMeanRpb, rpbEffectSize, repType, hpInfo, simpleRepeatInfo, tandemRepeatInfo, DP, FR, MT, UFR, sUMT_A, sUMT_T, sUMT_G, sUMT_C, logpval, FILTER, TnFetPval = line.strip().split('\t')
+         else:
+            CHROM, POS, REF, ALT, TYPE, sUMT, sForUMT, sRevUMT, sVMT, sForVMT, sRevVMT, sVMF, sForVMF, sRevVMF, VDP, VAF, RefForPrimer, RefRevPrimer, primerOR, pLowQ, hqUmiEff, allUmiEff, refMeanRpb, altMeanRpb, rpbEffectSize, repType, hpInfo, simpleRepeatInfo, tandemRepeatInfo, DP, FR, MT, UFR, sUMT_A, sUMT_T, sUMT_G, sUMT_C, logpval, FILTER = line.strip().split('\t')
          
          if TYPE == '0':
             continue
@@ -201,6 +276,7 @@ def makeVcf(runPath, outlong, sampleName, refGenome):
             fQUAL = float(QUAL)
          except ValueError:
             fQUAL = 0.00
+
 
          if fQUAL < minCutoff[TYPE.upper()]:
             lastCHROM = '.'
@@ -220,10 +296,25 @@ def makeVcf(runPath, outlong, sampleName, refGenome):
          currentAllele = (CHROM, POS, REF, ALT, TYPE, DP, VDP, VAF, sUMT, sVMT, sVMF, QUAL, fQUAL, FILTER)
          tempVar = (CHROM, POS, REF, ALT, TYPE, DP, VDP, VAF, sUMT, sVMT, sVMF, QUAL, FILTER)
          lenAlleles = len(alleles)
+         if tumorNormal:
+            currentAllele.append(TNFetPval)
+            tempVar.append(TNFetPval)
 
          if fQUAL < cutoff: ## Write to low-PI file
-            outLowPi.write('\t'.join([sampleName,CHROM,POS,".",REF,ALT,QUAL,FILTER,TYPE,RepRegion,DP,sUMT,sVMT,sVMF]))            
-            outLowPi.write('\n')
+
+            if tumorNormal:
+
+               if FILTER.find('LOH_HomRef') != -1:
+                  FILTER.replace('_HomRef', '')
+                  ALT = '.'
+
+               outLowPi.write('\t'.join([sampleName,CHROM,POS,".",REF,ALT,QUAL,FILTER,TYPE,TnFetPval,RepRegion,DP,sUMT,sVMT,sVMF]))
+               outLowPi.write('\n')
+
+            else:
+               outLowPi.write('\t'.join([sampleName,CHROM,POS,".",REF,ALT,QUAL,FILTER,TYPE,RepRegion,DP,sUMT,sVMT,sVMF]))
+               outLowPi.write('\n')
+
             continue
             
          # if current chrom and position equal to last line, append it for potential multi-allelic output
@@ -232,12 +323,12 @@ def makeVcf(runPath, outlong, sampleName, refGenome):
 
          # for new chrom or position, if last variant is not multi-allelic, write to vcf directly
          elif lenAlleles == 1:
-            biAllelicVar(alleles, RepRegion, outVcf, outVariants)
+            biAllelicVar(alleles, RepRegion, outVcf, outVariants, tumorNormal)
             alleles = [currentAllele]
 
          # if last variant is possible multi-allelic, combine and write as one 
          else:            
-            multiAllelicVar(alleles, RepRegion, outVcf, outVariants)
+            multiAllelicVar(alleles, RepRegion, outVcf, outVariants, tumorNormal)
             alleles = [currentAllele]
 
          lastCHROM, lastPOS = CHROM, POS
@@ -245,9 +336,9 @@ def makeVcf(runPath, outlong, sampleName, refGenome):
    # take care of the last line
    lenAlleles = len(alleles)
    if lenAlleles == 1:
-      biAllelicVar(alleles, RepRegion, outVcf, outVariants)
+      biAllelicVar(alleles, RepRegion, outVcf, outVariants, tumorNormal)
    elif lenAlleles >= 2:
-      multiAllelicVar(alleles, RepRegion, outVcf, outVariants)
+      multiAllelicVar(alleles, RepRegion, outVcf, outVariants, tumorNormal)
    else:
       pass
 
