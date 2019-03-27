0a1a769d13bf486b20c4d6f54e50d9c1d9f95831
rpadmanabhan <raghus606@gmail.com>
2019-03-27 14:07:35 +0000

    Update genome dir

--- tests/test_smcounterv2.sh
+++ tests/test_smcounterv2.sh
@@ -5,7 +5,7 @@ codedir=$1
 python $codedir/run.py --runPath /home/qiauser/test_v2/ --bamFile /home/qiauser/test_v2/NB956-240-3-10_S1.highconfidence.bam \
        --bedTarget /home/qiauser/test_v2/high.confidence.variants.bed --outPrefix NB956-240-3-10_S1.test --nCPU 2 --minBQ 25 \
        --minMQ 50 --hpLen 8 --mismatchThr 6.0 --primerDist 2 --consThr 0.8 --rpu 7.6 --primerSide 1 --minAltUMI 3 --maxAltAllele 2 \
-       --refGenome /srv/qgen/data/genome/ucsc.hg19.fa --repBed /srv/qgen/data/annotation/simpleRepeat.full.bed \
+       --refGenome /srv/qgen/data/genome/hg19/ucsc.hg19.fa --repBed /srv/qgen/data/annotation/simpleRepeat.full.bed \
        --srBed /srv/qgen/data/annotation/SR_LC_SL.full.bed
 
 python $codedir/tests/compare_outlong.py /home/qiauser/test_v2/intermediate/NB956-240-3-10_S1.test.VariantList.long.txt \
